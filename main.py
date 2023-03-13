6ebb233becde004db66d7ec8f3c6a52d05737d50
rkdruddud <rkdruddud1@naver.com>
2023-03-13 22:47:51 +0900

    connect db

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+
 from fastapi import FastAPI
 import uvicorn
 
+import pymysql
+from sqlalchemy import create_engine
 app = FastAPI()
+
+db_connection_str = 'mysql+pymysql://dev:nangok_devs@34.127.105.242/memberInfo'
+db_connection = create_engine(db_connection_str)
+conn=db_connection.connect()
+
+
 
 @app.get("/")
 def root():
