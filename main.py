a12b6375871d1e04478d375a60ff8c0a5abfdbae
rkdruddud <rkdruddud1@naver.com>
2023-03-13 23:11:20 +0900

    connect db

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import pymysql
 from sqlalchemy import create_engine
 app = FastAPI()
 
-db_connection_str = 'mysql+pymysql://dev:nangok_devs@34.127.105.242/memberInfo'
+## 데이터베이스 연결
+db_connection_str = 'mysql+pymysql://dev:nangok_devs@34.127.105.242/dropbybell'
 db_connection = create_engine(db_connection_str)
 conn=db_connection.connect()
 
